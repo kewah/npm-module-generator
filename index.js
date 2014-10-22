07293e58b66f65092214a456a9d96752ecce5ca9
Antoine Lehurt <keuwah@gmail.com>
2014-10-22 14:55:49 +0200

    Refactor test generator

--- index.js
+++ index.js
@@ -5,16 +5,28 @@ var readdirp = require('readdirp');
 var fs = require('fs-extra');
 var _ = require('underscore');
 _.mixin(require('underscore.string').exports());
+var utils = require('./lib/utils');
+
 
 require('./lib/prompt')(function(err, params) {
   if (err) {
     throw err;
   }
 
+  var manifests = [];
+  var testLibs = fs.readdirSync(path.join(__dirname, 'templates/test'));
+
   var stream = readdirp({
     root: path.join(__dirname, 'templates'),
     fileFilter: ['!.DS_Store'],
-    directoryFilter: ['!test']
+    // Exclude test templates that haven't been selected.
+    directoryFilter: testLibs
+      .filter(function(lib) {
+        return lib !== params.testlib;
+      })
+      .map(function(lib) {
+        return '!' + lib;
+      })
   });
 
   stream.on('data', function(file) {
@@ -25,62 +37,50 @@ require('./lib/prompt')(function(err, params) {
       return;
     }
 
-    fs.readFile(file.fullPath, 'utf8', function(err, data) {
-      if (err) {
-        throw err;
-      }
+    // Store the manifest to render it later (on stream end).
+    if (utils.isManifestFile(file)) {
+      manifests.push(file);
+      return;
+    }
 
+    utils.renderFile(file, params, function(data) {
       data = _.template(data)(params);
-
-      if (path.basename(file.name) === 'package.json') {
-        var json = JSON.parse(data);
-        var testFolder;
-
-        if (params.testlib !== 'none') {
-          testFolder = path.join(__dirname, 'templates/test', params.testlib);
-
-          _.extend(json, fs.readJsonSync(
-            path.join(testFolder, 'package.json')
-          ));
-
-          renderTestTemplate(testFolder, params);
-        }
-
-        fs.writeJson(dest, json);
-        return;
-      }
-
-      if (path.extname(file.name) === '.json') {
+      if (utils.isJsonFile(file)) {
         fs.writeJson(dest, JSON.parse(data));
         return;
       }
 
-      fs.writeFile(dest, data);
+      // Change the dest path for the test templates.
+      // From `test/{testlib}/filename.js` to `test/filename.js`.
+      if (/test/.test(file.path)) {
+        dest = file.path
+          .split(path.sep)
+          .filter(function(folder) {
+            return folder !== params.testlib;
+          })
+          .join(path.sep);
+      }
+
+      fs.outputFile(dest, data);
     });
   });
-});
 
-
-function renderTestTemplate(root, params) {
-  var stream = readdirp({
-    root: root,
-    fileFilter: ['!.DS_Store', '!package.json']
-  });
-
-  stream.on('data', function(file) {
-    var dest = path.resolve(process.cwd(), 'test', file.path);
-
+  stream.on('end', function() {
+    // Render package.json
+    var dest = path.resolve(process.cwd(), 'package.json');
     if (fs.existsSync(dest)) {
-      console.warn(file.path, 'already exists');
+      console.warn('package.json already exists');
       return;
     }
 
-    fs.readFile(file.fullPath, 'utf8', function(err, data) {
-      if (err) {
-        throw err;
-      }
-
-      fs.outputFile(dest, _.template(data)(params));
+    var manifest = {};
+    manifests.forEach(function(file) {
+      _.extend(
+        manifest,
+        JSON.parse(utils.renderFileSync(file, params))
+      );
     });
+
+    fs.writeJson(dest, manifest);
   });
-}
+});
