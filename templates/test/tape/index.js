307dc2f0eae99503283f2cbbb95438aec9c1dae8
Antoine Lehurt <keuwah@gmail.com>
2014-12-19 21:48:37 +0100

    Remove tape templates

--- templates/test/tape/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var tape = require('tape');
-var <%= _.camelize(moduleName) %> = require('..');
-
-tape('<%= moduleName %>', function(test) {
-  test.equal(true, true);
-  test.end();
-});
